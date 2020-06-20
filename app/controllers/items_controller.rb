c66adbb7ca4b3be7387175457a813b6b4322de5b
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-20 13:29:58 -0300

    método para o formulario de novo item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
-  def new
+  before_action :set_todo
+
+  def create
+    @item= @todo.items.create(item_params)
+    redirect_to @todo
+  end
+
+  private
+  def set_todo
+    @todo = Todo.find(params[:todo_id])
+  end
+  def item_params
+    params[:item].permit(:name)
   end
 end
