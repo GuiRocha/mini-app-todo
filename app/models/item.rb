adcad402c22a06b70a2f18b3ebd52d29b6585bee
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-21 14:39:47 -0300

    adicionando validações

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :todo
 
+  validates_presence_of :name, presence: true
+
   def completed?
     !completed_at.blank?
   end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,6 @@
 class Todo < ApplicationRecord
   has_many :items , dependent: :destroy
+
+  validates_presence_of :title, presence: true
+  validates_presence_of :description
 end
